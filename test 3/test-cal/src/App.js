d00a18213fbb5f44be9043fa2c7b83943f7ea768
gherage <gerich89@gmail.com>
2023-12-14 05:01:02 +0300

    releaseFix

--- test 3/test-cal/src/App.js
+++ test 3/test-cal/src/App.js
@@ -13,7 +13,43 @@ function App() {
 
   useEffect(() => {
     setCurrentMonth(getMonth(monthIndex));
+    Notification.requestPermission();
   }, [monthIndex]);
+
+  const events = JSON.parse(localStorage.getItem('savedEvents'));
+  events.forEach((event) => {
+    const timeParts = event.startTime.split(':');
+    const hours = parseInt(timeParts[0], 10);
+    const minutes = parseInt(timeParts[1], 10);
+    const eventTime = new Date();
+    eventTime.setHours(hours);
+    eventTime.setMinutes(minutes);
+
+    const notificationTime = new Date(eventTime.getTime() - 15 * 60000);
+
+    const notificationKey = `notification_${event.startTime}`;
+    const isNotificationShown = localStorage.getItem(notificationKey);
+    if (!isNotificationShown) {
+      const timeUntilNotification =
+        notificationTime.getTime() - new Date().getTime();
+      setTimeout(() => {
+        showNotification(event.title);
+        localStorage.setItem(notificationKey, 'true');
+      }, timeUntilNotification);
+    }
+  });
+
+  function showNotification(title) {
+    if (!('Notification' in window)) {
+      console.log('Браузер не поддерживает оповещения');
+      return;
+    }
+    if (Notification.permission === 'granted') {
+      new Notification('Оповещение', {
+        body: `Остается 15 минут до события: ${title}`,
+      });
+    }
+  }
 
   return (
     <React.Fragment>
